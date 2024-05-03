c442834379c9cfe7d5b813f207d3e1d64b1ef463
Haon <973491616@qq.com>
2024-05-04 01:15:22 +0800

    build project

---
+++ fps/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Room(models.Model):
+    name=models.CharField(default="Room",max_length=100)
+    port=models.IntegerField(default=7777)
+
+    def _str_(self):
+        return  self.name+'_'+str(self.port)
